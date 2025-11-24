37b987f41477ea5c05a32f9dbe12ffcc7b9c75fd
taufik.arifuddin <taufik.arifudidn@gdn-commerce.com>
2025-11-24 08:51:43 +0700

    add sample

---
+++ src/test/java/com/gdn/training/NumberHelperTest.java
@@ -0,0 +1,16 @@
+package com.gdn.training;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Number Helper Test")
+class NumberHelperTest {
+  @Test
+  @DisplayName("is odd test")
+  public void isOddTest() {
+    assertFalse(NumberHelper.isOdd(2));
+    assertTrue(NumberHelper.isOdd(3));
+  }
+}
