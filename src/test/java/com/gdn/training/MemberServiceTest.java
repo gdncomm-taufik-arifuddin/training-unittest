8aebea42f884bc0ea037c4ea247ff64b674f29ae
gdncomm-pristi-zahara <pristi.zahara@gdn-commerce.com>
2025-11-24 13:57:39 +0700

    Update MemberServiceTest.java

--- src/test/java/com/gdn/training/MemberServiceTest.java
+++ src/test/java/com/gdn/training/MemberServiceTest.java
@@ -3,7 +3,6 @@ package com.gdn.training;
 import com.gdn.training.dummy.entity.Member;
 import com.gdn.training.dummy.repository.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -15,10 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
